99dc9c706d379fe68c46c77ba73c00481f2ccb31
Prasanti <nalla.prasanti@gmail.com>
2023-02-01 16:53:36 +0000

    first version

---
+++ PhotoViewer/src/photoViewer/imageselector.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './imageselector.css';
+
+const brokenImages = [
+    1, 24, 32, 36, 44, 47
+];
+function ImageSelector() {
+    const urls = [];
+
+    for (let i = 0; i < 50; i++) {
+        if (!brokenImages.includes(i)) {
+            const imageNumberString = i.toString().padStart(2, '0');
+            urls.push(`https://picsum.photos/id/6${imageNumberString}/1600/900.jpg`)
+        }
+    }
+
+    return urls;
+}
+export const imageSelection = ImageSelector();
