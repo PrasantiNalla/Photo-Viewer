4da17d100959a7e53b43f0a1e24c06e3b820cacc
Prasanti <nalla.prasanti@gmail.com>
2023-02-02 12:37:47 +0000

    working image preview without border

--- PhotoViewer/src/App.tsx
+++ PhotoViewer/src/App.tsx
@@ -6,23 +6,19 @@ import { imageUrls } from './photoViewer/imageselector';
 
 
 function App() {
-  let [image, setImage] = useState();
-
-
-  function previewImage() {
-
-
-  }
+  let [image, setImage] = useState(imageUrls[5]);
 
   return (
     <div>
       <h1>React Photo Viewer</h1>
-      <PhotoViewer url="https://picsum.photos/id/600/1600/900.jpg" />
+      <div>
+        <PhotoViewer url={image} />
+      </div>
       <h3 className='grid'>Select a image to preview</h3>
       <div className='child'>
         {imageUrls.map((url) => {
           return (
-            <img key={url} src={url} onClick={previewImage} />
+            <img key={url} src={url} onClick={() => setImage(url)} />
           )
         })}
       </div>
